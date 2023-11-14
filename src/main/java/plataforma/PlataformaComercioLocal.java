424f1e51d92fd891956697e6aa0ebdad47b126bc
Emilio Gordillo <gor18062@uvg.edu.gt>
2023-11-13 22:02:17 -0600

    Se agrego el script preeliminar para la conexcion con sqlite

--- src/main/java/plataforma/PlataformaComercioLocal.java
+++ src/main/java/plataforma/PlataformaComercioLocal.java
@@ -4,12 +4,16 @@ package main.java.plataforma;
 import main.java.plataforma.modelo.Plataforma;
 import main.java.plataforma.controlador.Controlador;
 import main.java.plataforma.vista.VistaGUI;
+import main.java.plataforma.database.SQLiteDBConnector;
 
 public class PlataformaComercioLocal {
     public static void main(String[] args) {
         Plataforma plataforma = new Plataforma();
         VistaGUI vista = new VistaGUI(new Controlador(null, plataforma));
         Controlador controlador = new Controlador(vista, plataforma);
+        SQLiteDBConnector sqLiteDBConnect = new SQLiteDBConnector();
         vista.setVisible(true);  // Mostramos la ventana principal
+
+
     }
 }
--- src/main/java/plataforma/database/SQLiteDBConnector.java
+++ src/main/java/plataforma/database/SQLiteDBConnector.java
@@ -4,10 +4,31 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SQLiteDBConnector {
-    private static final String URL = "jdbc:sqlite:path_to_your_database.db";
+public class SQLiteDBConnector{
 
-    public static Connection connect() throws SQLException {
-        return DriverManager.getConnection(URL);
+    public static void connect(String filename)  {
+        Connection conn = null;
+        String URL = "D:/Documentos/UVG/Mate/Semestre 1/POO/POO P2/Proyecto_2/Database/" + filename;
+        try {
+            conn = DriverManager.getConnection(URL);
+            System.out.println("Connection to SQLite has been established.");
+        }catch (SQLException e ) {
+            System.out.print(e.getMessage());
+
+        }finally {
+            try{
+                if (conn != null){
+                    conn.close();
+                }
+            }catch (SQLException ex){
+                System.out.print(ex.getMessage());
+            }
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        connect("Database.db");
     }
 }
